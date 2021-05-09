1f4288255731ffdf0f7c0097df122404c082757e
ivVane <iv.vane@gmail.com>
2021-05-09 16:02:42 +0200

    Creating the model data classes

---
+++ app/src/main/java/com/vane/android/addmaptoandroidapp/place/PlacesReader.kt
@@ -0,0 +1,24 @@
+package com.vane.android.addmaptoandroidapp.place
+
+import android.content.Context
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import com.vane.android.addmaptoandroidapp.R
+import java.io.InputStream
+import java.io.InputStreamReader
+
+class PlacesReader(private val context: Context) {
+
+    private val gson = Gson()
+
+    private val inputStream: InputStream
+        get() = context.resources.openRawResource(R.raw.places)
+
+    fun read(): List<Place> {
+        val itemType = object : TypeToken<List<PlaceResponse>>() {}.type
+        val reader = InputStreamReader(inputStream)
+        return gson.fromJson<List<PlaceResponse>>(reader, itemType).map {
+            it.toPlace()
+        }
+    }
+}
