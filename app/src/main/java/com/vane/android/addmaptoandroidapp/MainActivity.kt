34d320671e14b1c5c13be6ac94e5b0aca160838d
ivVane <iv.vane@gmail.com>
2021-05-09 22:06:36 +0200

    Adding markers to the map

--- app/src/main/java/com/vane/android/addmaptoandroidapp/MainActivity.kt
+++ app/src/main/java/com/vane/android/addmaptoandroidapp/MainActivity.kt
@@ -2,10 +2,41 @@ package com.vane.android.addmaptoandroidapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.MarkerOptions
+import com.vane.android.addmaptoandroidapp.place.Place
+import com.vane.android.addmaptoandroidapp.place.PlacesReader
 
 class MainActivity : AppCompatActivity() {
+
+    private val places: List<Place> by lazy {
+        PlacesReader(this).read()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Reference to GoogleMap
+        val mapFragment = supportFragmentManager.findFragmentById(
+            R.id.map_fragment
+        ) as? SupportMapFragment
+        mapFragment?.getMapAsync { googleMap ->
+            addMarkers(googleMap)
+        }
+    }
+
+    /**
+     * Adds marker representations of the places list on the provided GoogleMap object
+     */
+    private fun addMarkers(googleMap: GoogleMap) {
+        places.forEach { place ->
+            val marker = googleMap.addMarker(
+                MarkerOptions()
+                    .title(place.name)
+                    .position(place.latLng)
+            )
+        }
     }
 }
